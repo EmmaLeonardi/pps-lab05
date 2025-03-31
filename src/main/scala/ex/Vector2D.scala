2772e4813b89188dc61febe14333ca91b59f798e
Emma Leonardi - emma.leonardi2@studio.unibo.it <emma.leonardi2@studio.unibo.it>
2025-03-31 10:48:07 +0200

    Implemented Vector2D

--- src/main/scala/ex/Vector2D.scala
+++ src/main/scala/ex/Vector2D.scala
@@ -25,7 +25,7 @@ trait Vector2D:
 
 object Vector2D:
   // Factory method to create Vector2D instances
-  def apply(x: Double, y: Double): Vector2D = ???
+  def apply(x: Double, y: Double): Vector2D = Vector2DImpl(x,y)
 
   // Common vectors (optional but nice)
   val zero: Vector2D = apply(0.0, 0.0)
@@ -40,6 +40,27 @@ object Vector2D:
  *   - Modify the implementation to use a `case class Vector2DImpl` instead.
  *   - Observe how equality (==) and toString now work correctly out-of-the-box.
  */
+
+case class Vector2DImpl(x1: Double, y1:Double) extends Vector2D:
+  def x: Double = x1
+  def y: Double = y1
+
+  // Vector addition: (x1, y1) + (x2, y2) = (x1+x2, y1+y2)
+  def +(other: Vector2D): Vector2D = Vector2D(other.x+this.x, other.y+this.y)
+
+  // Vector subtraction: (x1, y1) - (x2, y2) = (x1-x2, y1-y2)
+  def -(other: Vector2D): Vector2D = Vector2D(this.x-other.x, this.y-other.y)
+
+  // Scalar multiplication: s * (x, y) = (s*x, s*y)
+  def *(scalar: Double): Vector2D = Vector2D(scalar*this.x, scalar*this.y)
+
+  // Dot product: (x1, y1) · (x2, y2) = x1*x2 + y1*y2
+  def dot(other: Vector2D): Double = other.x*this.x + other.y*this.y
+
+  // Magnitude (length): ||(x, y)|| = sqrt(x*x + y*y)
+  def magnitude: Double = sqrt(this.x*this.x + this.y*this.y)
+
+
 @main def checkVectors(): Unit =
   val v1 = Vector2D(3.0, 4.0)
   val v2 = Vector2D(-1.0, 2.0)
@@ -69,8 +90,8 @@ object Vector2D:
   println(s"Magnitude of v2: $magV2") // Check if close to 2.236
 
   // Check zero vector and unit vectors if implemented in companion object
-  // println(s"Zero vector: ${Vector2D.zero}")
-  // println(s"Dot product v1.dot(Vector2D.i): ${v1.dot(Vector2D.i)}") // Should be v1.x = 3.0
+  println(s"Zero vector: ${Vector2D.zero}")
+  println(s"Dot product v1.dot(Vector2D.i): ${v1.dot(Vector2D.i)}") // Should be v1.x = 3.0
 
   val multipleOps = (v1 + v2) * 3.0 - Vector2D(1.0, 1.0)
   // sum = (2.0, 6.0)
