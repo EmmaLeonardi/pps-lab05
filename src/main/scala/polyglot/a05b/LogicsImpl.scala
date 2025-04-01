774a160374d66e6834b375cedaadb0cfa4d1fc9b
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2025-04-01 19:09:24 +0200

    Implemented logicsImpl in a05b

--- src/main/scala/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/polyglot/a05b/LogicsImpl.scala
@@ -2,10 +2,20 @@ package polyglot.a05b
 
 import polyglot.a05b.Logics
 
+import scala.math.abs
+
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
+  private val dim = size
+  private val random = scala.util.Random
+  private val initialPosition = (random.nextInt(dim - 2) + 1, random.nextInt(dim - 2) + 1)
+  private var counter = 0
 
-  override def isOver: Boolean = false
+  override def tick(): Unit = counter = counter + 1
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def isOver: Boolean =
+    initialPosition._2 - counter < 0 || initialPosition._2 + counter >= dim || initialPosition._1 - counter < 0 || initialPosition._1 + counter >= dim
+
+  override def hasElement(x: Int, y: Int): Boolean =
+    (x == initialPosition._1 && abs(y - initialPosition._2) <= counter) || (y == initialPosition._2 && abs(x - initialPosition._1) <= counter) |
+      (x - y == initialPosition._1 - initialPosition._2 && abs(x - initialPosition._1) <= counter) || (x + y == initialPosition._1 + initialPosition._2 && abs(y - initialPosition._2) <= counter)
